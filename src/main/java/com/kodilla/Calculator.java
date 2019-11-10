94a48bd33204186c9375b1375fb4c6450c353563
paluchowskitomasz <paluchowski.tomasz@gmail.com>
2019-11-10 08:59:08 +0100

    Class Calculator added!

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,35 @@
 package com.kodilla;
 
 public class Calculator {
+
+    private int a;
+    private int b;
+
+    public Calculator(){
+        this.a = a;
+        this.b = b;
+    }
+    public int getA(){
+
+        return a;
+    }
+
+    public int getB(){
+
+        return b;
+    }
+
+    public int Dodaj(int a, int b){
+
+        return a + b;
+
+    }
+
+    public int Odejmij(int a, int b){
+
+        return a - b;
+
+    }
+
+
 }
